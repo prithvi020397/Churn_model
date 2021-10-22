739456599686f80b68e73f9f9672ec019bba3325
Prithvi Telkpally <prithvisai@Prithvis-MacBook-Pro.local>
2021-10-22 10:36:46 -0500

    Adding cookiecutter template

---
+++ churn_model/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to End Machine Learning pipelinewith MLOps tools',
+    author='prithvi_telkapally',
+    license='MIT',
+)
